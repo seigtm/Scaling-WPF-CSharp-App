3b8d63938e022d1d6a33bf1363ad9c9871234961
Костыль <seigtm@gmail.com>
2021-05-20 20:33:46 +0300

    Initial commit

---
+++ Scaling/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace ScalingNamespace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const uint DefaultHeight = 300;
+        private const uint DefaultWidth = 500;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            Scaling scaling = new(this);
+            _ = scaling.ShowDialog();
+        }
+
+        public void ChangeScaling(uint scaling)
+        {
+            Height = DefaultHeight * scaling / 100;
+            Width = DefaultWidth * scaling / 100;
+        }
+    }
+}
---
+++ Scaling/Scaling.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace ScalingNamespace
+{
+    /// <summary>
+    /// Логика взаимодействия для Scaling.xaml
+    /// </summary>
+    public partial class Scaling : Window
+    {
+        private readonly MainWindow w;
+
+        public Scaling(MainWindow window)
+        {
+            w = window;
+            InitializeComponent();
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            w.ChangeScaling(ScalingAmount);
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            w.ChangeScaling(ScalingAmount);
+            DialogResult = true;
+        }
+
+        public uint ScalingAmount => Convert.ToUInt32(scalingBox.Text);
+    }
+}
